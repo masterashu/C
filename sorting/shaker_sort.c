1596256d50b3bf63fa447162f1adf4f8e66cb6fd
khare-agraj <khare.agraj@gmail.com>
2021-10-22 22:47:53 +0530

    Update to Shaker Sort

--- sorting/shaker_sort.c
+++ sorting/shaker_sort.c
@@ -1,35 +1,42 @@
-#include <stdio.h>
+#include  <stdio.h>  
 
-void swap(int *a, int *b){
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *x, int *y) {
+	if(x == y)
+		return;
+	*x ^= *y;
+	*y ^= *x;
+	*x ^= *y;
 }
-void shakersort(int a[], int n)
-{
-    int p, i;
-    for (p = 1; p <= n / 2; p++)
-    {
-        for (i = p - 1; i < n - p; i++)
-            if (a[i] > a[i+1]){
-                swap(&a[i], &a[i + 1]);
+void cocktailsort(int *a, size_t n) {
+	while(1) {
+		char flag;
+		int it, i;
+		size_t start[2] = {1, n - 1},
+			   end[2] = {n, 0},
+			   inc[2] = {1, -1};
+		for(it = 0; it < 2; ++it) {
+			flag = 1;
+			for(i = start[it]; i != end[it]; i += inc[it])
+				if(a[i - 1] > a[i]) {
+					swap(a + i - 1, a + i);
+					flag = 0;
+				}
+			if(flag)
+				return;
 		}
-        for (i = n - p - 1; i >= p; i--)
-            if (a[i] < a[i-1]){
-                swap(&a[i], &a[i - 1]);
-		}
-    }
+	}
 }
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    int arr[n] ,i;
-    for (i = 0 ; i < n; i++)
-        scanf("%d ", &arr[i]);
-    shakersort(arr, n);
-    for (i = 0 ; i < n; i++)
-        printf("%d ", arr[i]);
-    return 0;
+ 
+int main(void) {
+	int a[] = { 5, -1, 101, -4, 0, 1, 8, 6, 2, 3 };
+	int i;
+	size_t n = sizeof(a)/sizeof(a[0]);
+    printf("Original Array:\n");
+    for (i = 0; i < n; i++)
+    printf("%d%s", a[i], i == n - 1 ? "\n" : " ");  
+    printf("\nSorted Array:\n");
+	cocktailsort(a, n);
+	for ( i = 0; i < n; ++i)
+		printf("%d ", a[i]);
+	return 0;
 }
